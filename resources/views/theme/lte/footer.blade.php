211da708024eee23232efcf07a498505bff475b4
CursoLaravel <rusbel_ariza@hotmail.com>
2019-08-25 16:01:52 -0500

    Template Blade

---
+++ resources/views/theme/lte/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 2.0
+    </div>
+    <strong>Copyright &copy; 2019 
+        <a href="https://adminlte.io">SIGESA</a>.</strong> Rusbel Ariza Bravo.
+</footer>
